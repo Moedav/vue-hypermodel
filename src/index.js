241ade9fa7f74e9fc4f9a6aafad1362d86ad5216
Moedav <david.moedl@gmail.com>
2020-09-28 09:21:51 +0200

    export link parser

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Model from './model'
 import Relationship from './relationships'
 import Store from './store'
 import Transformer from './transformers'
+import parseLinks from './link-header-parser'
 
 export default Store
 export {
   Model,
   Relationship,
-  Transformer
+  Transformer,
+  parseLinks
 }
